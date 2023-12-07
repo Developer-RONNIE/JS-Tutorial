1490aa37177bd065d3f723732e19ae9ecdb19379
Developer-RONNIE <ronnieworkspace@gmail.com>
2023-12-07 23:51:38 +0530

    history & development of JS

--- test.js
+++ test.js
@@ -1,1 +1,19 @@
+
 console.log("Hello World");
+
+
+// ********** HISTORY OF JS & DEVELOPMENT  ******
+
+
+/* Which file can be injected into a software and executed ? 
+
+- In case of JavaScript the software was hidden in Browser itself 
+- this was the main reason why JavaScript was used with index.html 
+- Only Senior developers worked with JavaScript in the browser by itself as it was a tidious process 
+- later on this software was extracted from the browser and Standalone JavaScript was introduced 
+
+- Some of the fomously know environments for JavaScript nowadays are : 
+- node.js 
+- deno.js 
+- Founder name : Ryan Dahl
+*/
