556177c38d7b2e526c7a512726d727658cbcb8bf
Developer-RONNIE <ronnieworkspace@gmail.com>
2024-01-02 14:26:23 +0530

    While Loops + Do-While Loops + While Loops In Array

---
+++ 05_iteration/02_loops.js
@@ -0,0 +1,27 @@
+// +++++++++++++++++  While Loop +++++++++++++++++
+
+let index = 0 
+while (index <= 10){
+    // console.log(`Value of index is ${index}`);
+    index = index + 2
+}
+
+
+
+// While Loop in Array 
+let myArray = ["flash", "batman", "robin"]
+
+let arr = 0
+while (arr < myArray.length){
+    // console.log(`Value is ${myArray[arr]}`);
+    arr = arr +1
+}
+
+
+//  Do-While Loop 
+let score = 0 
+// let score = 11  //output: Score is 11
+do {
+    console.log(`Score is ${score}`);
+    score ++
+} while (score <= 5);
