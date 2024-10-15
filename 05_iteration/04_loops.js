2388672c4f0f8d4a4e7d0474d6d005ca9942a4de
Developer-Ronnie <ronnieworkspace@gmail.com>
2024-10-15 05:31:20 +0530

    filter + map + filter real world example

--- 05_iteration/04_loops.js
+++ 05_iteration/04_loops.js
@@ -10,7 +10,7 @@ const myNums = [1,2,3,4,5,6,7,8,9,10]
 // })
 // console.log(newNums);
 
-// ** How to use forEach instead in filter 
+// ** How to use forEach instead of filter 
 const newNums = []  // empty array 
 myNums.forEach((num) => {
     if(num > 4){
